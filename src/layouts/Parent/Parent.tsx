2733ba9d5958fc119d213f96889b08ba19ed4e1c
voiddata <sasidhar6998@gmail.com>
2023-05-02 17:25:23 +0000

    added minimize and maximize functionality

--- src/layouts/Parent/Parent.tsx
+++ src/layouts/Parent/Parent.tsx
@@ -1,14 +1,29 @@
+import { MouseEventHandler, MouseEvent, useState } from 'react';
 import Buttons from '../Buttons/Buttons';
 import Display from '../Display/Display';
 import Titlebar from '../Titlebar/Titlebar';
 import styles from './Parent.module.css';
 
 function Parent(): JSX.Element {
-    return <div className={styles.Parent}>
-        <Titlebar />
-        <Display />
-        <Buttons />
-    </div>;
+    const [minimize, setMinimize]: [boolean, React.Dispatch<React.SetStateAction<boolean>>] = useState(true);
+
+    const minimizeClickHandler: MouseEventHandler<HTMLDivElement> = (event: MouseEvent): void => {
+        setMinimize((prev) => {
+            return !prev
+        });
+    };
+
+    const parentStyleClass = styles.Parent + ` ${minimize === false ? styles['Parent-mazimize'] : styles['Parent-minimize']}`;
+
+    return (
+        <div className={parentStyleClass}>
+            <Titlebar
+                minimizeClickHandler={minimizeClickHandler}
+            />
+            <Display />
+            <Buttons />
+        </div>
+    );
 }
 
 export default Parent;
--- src/layouts/Titlebar/Titlebar.tsx
+++ src/layouts/Titlebar/Titlebar.tsx
@@ -1,23 +1,30 @@
 import styles from './Titlebar.module.css';
+import { MouseEventHandler } from 'react';
 
-function Titlebar(): JSX.Element {
-    return <div className={styles.Titlebar}>
-        <div className={styles['icon-and-title-div']}>
-            <img src="calc.png" className={styles['calc-icon']} alt='calculator icon' />
-            <label className={styles.title}>Calculator</label>
+type TitlebarProps = {
+    minimizeClickHandler: MouseEventHandler<HTMLDivElement>
+}
+
+function Titlebar(props: TitlebarProps): JSX.Element {
+    return (
+        <div className={styles.Titlebar}>
+            <div className={styles['icon-and-title-div']}>
+                <img src="calc.png" className={styles['calc-icon']} alt='calculator icon' />
+                <label className={styles.title}>Calculator</label>
+            </div>
+            <div className={styles.buttons}>
+                <div className={styles['minimize-button-div']}>
+                    <input type='image' alt='minimize' src='icons8-subtract-64.png' className={styles['minimize-button']} />
+                </div>
+                <div className={styles['maximize-button-div']} onClick={props.minimizeClickHandler}>
+                    <input type='image' alt='maximize' src='maximize.png' className={styles['maximize-button']} />
+                </div>
+                <div className={styles['close-button-div']}>
+                    <input type='image' alt='close' src='Close-134.png' className={styles['close-button']} />
+                </div>
+            </div>
         </div>
-        <div className={styles.buttons}>
-            <div className={styles['minimize-button-div']}>
-                <input type='image' alt='minimize' src='icons8-subtract-64.png' className={styles['minimize-button']} />
-            </div>
-            <div className={styles['maximize-button-div']}>
-                <input type='image' alt='maximize' src='maximize.png' className={styles['maximize-button']} />
-            </div>
-            <div className={styles['close-button-div']}>
-                <input type='image' alt='close' src='Close-134.png' className={styles['close-button']} />
-            </div>
-        </div>
-    </div>;
+    );
 }
 
 export default Titlebar;
